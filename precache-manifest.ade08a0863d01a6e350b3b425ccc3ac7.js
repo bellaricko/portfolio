204315db76e2bb4b64c42efa770be905b9fa7bc2
Izabella Lönnqvist <izabellalonnqvist@nl114-111-162.student.uu.se>
2019-01-04 11:39:17 +0100

    Updates

---
+++ precache-manifest.ade08a0863d01a6e350b3b425ccc3ac7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "edeff788bfc50d2b5772",
+    "url": "/bellaricko.github.io/static/css/main.2d8fdc95.chunk.css"
+  },
+  {
+    "revision": "edeff788bfc50d2b5772",
+    "url": "/bellaricko.github.io/static/js/main.edeff788.chunk.js"
+  },
+  {
+    "revision": "fcbf51108b7c857e677c",
+    "url": "/bellaricko.github.io/static/js/1.fcbf5110.chunk.js"
+  },
+  {
+    "revision": "2c2bcc14d2e85fcd43f9",
+    "url": "/bellaricko.github.io/static/js/runtime~main.2c2bcc14.js"
+  },
+  {
+    "revision": "4c42dc0ceebaab777bad41d0a02de8e0",
+    "url": "/bellaricko.github.io/index.html"
+  }
+];
